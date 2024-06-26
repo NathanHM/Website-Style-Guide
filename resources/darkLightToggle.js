66b3d4cc6d44f5c314e75059454e5765231ae519
NathanHM <NathanHM16@gmail.com>
2024-06-26 18:53:13 +0100

    Theme switch designed

--- resources/darkLightToggle.js
+++ resources/darkLightToggle.js
@@ -5,7 +5,19 @@ function loadToggle() {
     }
 }
 
+
 function toggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    toggleImage();
+}
+
+function toggleImage() {
+    let initImg = document.getElementById("theme-icon").src;
+    if (initImg.includes("resources/darkIcon.png")) {
+        document.getElementById("theme-icon").src = "resources/lightIcon.png";
+    }
+    else {
+        document.getElementById("theme-icon").src = "resources/darkIcon.png";
+    }
 }
