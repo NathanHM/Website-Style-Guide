58121785958cbffdd1c3894bbd178f598eccc110
NathanHM <NathanHM16@gmail.com>
2024-06-26 13:45:15 +0100

    Added auto toggle for theme

--- resources/darkLightToggle.js
+++ resources/darkLightToggle.js
@@ -1,3 +1,14 @@
+function loadToggle() {
+    if (window.matchMedia) {
+        //alert("Match media working.")
+    }
+    
+    if (darkTheme.matches === false) {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+    }
+}
+
 function toggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
