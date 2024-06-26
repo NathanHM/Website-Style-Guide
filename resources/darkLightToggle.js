474037d99dd7658bc50480d0033f618a40d86d12
NathanHM <NathanHM16@gmail.com>
2024-06-26 13:51:20 +0100

    Cleaned code

--- resources/darkLightToggle.js
+++ resources/darkLightToggle.js
@@ -1,8 +1,4 @@
 function loadToggle() {
-    if (window.matchMedia) {
-        //alert("Match media working.")
-    }
-    
     if (darkTheme.matches === false) {
         var element = document.body;
         element.classList.toggle("dark-mode");
